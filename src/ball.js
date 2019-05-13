ecdca9f605d22bae3cf273eb2e53a52e74329182
Joren Verbaandert <jorenverbaandert@hotmail.com>
2019-05-13 22:20:57 +0200

    move createBalls to ball.js

---
+++ src/ball.js
@@ -0,0 +1,59 @@
+import * as vec2 from 'gl-vec2';
+
+export function createBall(table, color, number, col = 0, row = 0, half = false) {
+  const x = col * 27;
+  const y = row * 27;
+
+  const ball = {
+    position: vec2.fromValues(
+      table.inner.x + 3 * (table.inner.width / 4) + x,
+      table.inner.y + (table.inner.height / 2) + y,
+    ),
+    get x() {
+      return this.position[0];
+    },
+    get y() {
+      return this.position[1];
+    },
+    color,
+    half,
+    number,
+  };
+  return ball;
+}
+
+export function createBalls(table) {
+  const balls = [];
+
+  const whiteBall = {
+    position: vec2.fromValues(
+      table.inner.x + (table.inner.width / 4),
+      table.inner.y + (table.inner.height / 2),
+    ),
+    get x() {
+      return this.position[0];
+    },
+    get y() {
+      return this.position[1];
+    },
+    color: 'white',
+  };
+  balls.push(whiteBall);
+
+  balls.push(createBall(table, 'black', 8));
+  balls.push(createBall(table, 'yellow', 1, -2));
+  balls.push(createBall(table, 'yellow', 9, -1, 0.5, true));
+  balls.push(createBall(table, 'green', 14, -1, -0.5, true));
+  balls.push(createBall(table, 'blue', 2, 0, -1));
+  balls.push(createBall(table, 'green', 6, 0, 1));
+  balls.push(createBall(table, 'orange', 13, 1, -1.5, true));
+  balls.push(createBall(table, 'darkred', 15, 1, -0.5, true));
+  balls.push(createBall(table, 'darkred', 7, 1, 0.5));
+  balls.push(createBall(table, 'blue', 10, 1, 1.5, true));
+
+  balls.push(createBall(table, 'orange', 5, 2, -2));
+  balls.push(createBall(table, 'purple', 12, 2, -1, true));
+  balls.push(createBall(table, 'purple', 4, 2, 0));
+  balls.push(createBall(table, 'red', 11, 2, 1, true));
+  balls.push(createBall(table, 'red', 3, 2, 2));
+}
