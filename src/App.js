bea75528e67b63b50e815fb66108a122f9774509
MaartenBerns <maarten.berns@ogd.nl>
2019-05-15 10:18:02 +0200

    Add logo; Add new env map

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import React from 'react';
 import './App.css';
 import Canvas from "./canvas";
 
+import logo from './images/logo.jpg'; 
 
 function App() {
   return (
     <div className="App">
+      <img src={logo} alt="Logo" />
       <Canvas/>
     </div>
   );
